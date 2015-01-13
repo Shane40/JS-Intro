f4bd0505accf6d71df28a33faee4568bb0c993b6
Shane Blatt <shane.blatt@yahoo.com>
2015-01-13 15:20:02 -0500

    Answers to HW

---
+++ main.js
@@ -0,0 +1,53 @@
+var name;
+// Q: What does name equal right now?
+Undefined
+
+name = "Tim";
+// Q: What is `name` set to right now?
+Tim
+
+name = "Bill";
+// Q: What is `name` set to right now?
+Bill
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+Hello, how are you?
+
+var high = 50;
+var low  = 10;
+
+var math = high - low;
+// Q: What is `math` set to?
+40
+
+math = high - "5";
+// Q: What is `math` set to?
+45
+
+// Create a variable to calculate my age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+
+var born = 1983;
+var today = 2015;
+var Timsage = today - born;
+Timsage
+32
+var agestatement = "Tim is " + Timsage + " years old";
+agestatement
+"Tim is 32 years old"
+
+// Store some information following in variables.
+var yourname;
+var myname;
+
+var statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname;
+var Shane;
+var Tim;
+var yourname = "Shane";
+var myname = "Tim";
+var statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname
+statement
+"Shane is taking a class at The Iron Yard, and my instructor is Tim"
